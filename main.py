33537c20554ff25b427b10e0f52108ef8acdb00c
Leonardo Febres <febres35@gmail.com>
2023-06-11 23:33:13 -0400

    cambios para subir al Git

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.server.instance import server
 from src.controllers.consulta import * 
-from src.controllers.averia import *
+
 
 server.api.add_namespace(ns_consulta)
-server.api.add_namespace(ns_averia)
+
 
 server.run()
 
